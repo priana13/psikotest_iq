85ed2a6cc8a6bc368a55faa958d1b70da457b2c2
priana13 <herian.sap@gmail.com>
2022-11-05 23:58:21 +0700

    Home page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,7 +34,7 @@
             <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-laugh-wink"></i>
             </div>
-            <div class="sidebar-brand-text mx-3">Psikotes Online</div>
+            <div class="sidebar-brand-text mx-3">Psikotes</div>
         </a>
 
         <!-- Divider -->
