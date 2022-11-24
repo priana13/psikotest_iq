4abb34ddca3667ce499bdeea179e499b2d6ba10e
priana13 <herian.sap@gmail.com>
2022-11-25 05:57:02 +0700

    hide alert

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -195,7 +195,7 @@
                     </li>
 
                     <!-- Nav Item - Alerts -->
-                    <li class="nav-item dropdown no-arrow mx-1">
+                    <li class="nav-item dropdown no-arrow mx-1 d-none">
                         <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-bell fa-fw"></i>
                             <!-- Counter - Alerts -->
@@ -293,7 +293,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Alejandro RH {{ now()->year }}</span>
+                    <span>Copyright &copy; Psikotes {{ now()->year }}</span>
                 </div>
             </div>
         </footer>
--- resources/views/layouts/admin_full.blade.php
+++ resources/views/layouts/admin_full.blade.php
@@ -96,7 +96,7 @@
                     </li>
 
                     <!-- Nav Item - Alerts -->
-                    <li class="nav-item dropdown no-arrow mx-1">
+                    <li class="nav-item dropdown no-arrow mx-1 d-none">
                         <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-bell fa-fw"></i>
                             <!-- Counter - Alerts -->
