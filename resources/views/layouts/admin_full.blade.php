61056c3b548fad6a6c520273c19436ab28df0b03
priana13 <herian.sap@gmail.com>
2022-12-21 13:35:39 +0700

    ganti nama copyright

--- resources/views/layouts/admin_full.blade.php
+++ resources/views/layouts/admin_full.blade.php
@@ -189,7 +189,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    {{-- <span>Copyright &copy; PondokWeb {{ now()->year }}</span> --}}
+                    
                 </div>
             </div>
         </footer>
