99f3720eba4c166293c85776583977261a4179a0
priana13 <herian.sap@gmail.com>
2024-01-30 21:51:48 +0700

    upper case icon

--- resources/views/components/membership-icon.blade.php
+++ resources/views/components/membership-icon.blade.php
@@ -1,4 +1,4 @@
 <div class="nav-item ml-2 my-auto {{ (!$paket_aktif) ? 'd-none' : '' }}">  
 
-    <a class="btn btn-outline-warning text-dark" disabled>{{ ($paket_aktif) ? $paket_aktif->name : "" }}</a>
+    <a class="btn btn-outline-warning text-dark" disabled>{{ ($paket_aktif) ? strtoupper( $paket_aktif->name ) : "" }}</a>
 </div>
