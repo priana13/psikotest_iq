6b6789e40f8c9dda8476808ea57110fe1a3f94ad
priana13 <herian.sap@gmail.com>
2024-02-03 19:21:18 +0700

    ckeditor cdn

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -283,7 +283,9 @@
 <script src="{{asset('vendor/bootstrap-toastr/toastr.min.js')}}" type="text/javascript"></script>
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
-<script src="{{ asset('vendor/ckeditor/ckeditor.js') }}"></script>
+{{-- <script src="{{ asset('vendor/ckeditor/ckeditor.js') }}"></script> --}}
+<script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script>
+<script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js"></script>   
 
