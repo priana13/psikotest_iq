de72e161238a2080fde524605236b0233ead7dcb
priana13 <herian.sap@gmail.com>
2024-02-20 06:00:42 +0700

    offline email completed

---
+++ resources/views/mail/completed-offline-order-mail.blade.php
@@ -0,0 +1,13 @@
+@component('mail::message')
+
+
+Terima kasih telah melakukan pembayaran. 
+
+Anda sudah terdata secara otomatis dalam sistem. 
+
+Informasi ini selanjutnya sebagai bukti yang sah registrasi Anda. Jika diperlukan, silahkan hubungi Kami untuk konfirmasi.
+
+Terimakasih,<br>
+
+{{ config('app.name') }}
+@endcomponent
