3a19b8e95e9ac59d8aa1094faf8af178460b1e26
priana13 <herian.sap@gmail.com>
2024-03-25 21:00:34 +0700

    tambah table tryout exams

---
+++ database/migrations/2024_03_25_205242_create_tryout_exams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTryoutExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tryout_exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Kecerdasan , Kepribadian , Sikap Kerja
+            $table->unsignedBigInteger('exam_id');
+
+            $table->foreign('exam_id')->references('id')->on('exams')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tryout_exams');
+    }
+}
