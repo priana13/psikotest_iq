2ca4e6659df6280b66a3e255a741e338520c10d0
priana13 <herian.sap@gmail.com>
2024-12-16 21:14:19 +0700

    kosongkan dashboard filament

---
+++ database/seeders/UsiaKunciNormaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\KunciNorma;
+use Illuminate\Database\Seeder;
+
+class UsiaKunciNormaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $kunci_norma = KunciNorma::get();
+
+        foreach ($variable as $key => $value) {
+            # code...
+        }
+    }
+}
